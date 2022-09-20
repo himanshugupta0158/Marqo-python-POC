b271bce10806926b52752b6da73c88dbc6ad9790
himanshugupta0158 <himanshugupta0158@gmail.com>
2022-09-20 21:23:16 +0530

    searching by index name

--- script3.py
+++ script3.py
@@ -4,8 +4,14 @@ import pprint
 mq = marqo.Client(url='http://localhost:8882')
 
 
+
+
+pprint.pprint(mq.index("my-first-index").get_stats())
+pprint.pprint(mq.index("my-first-index").refresh())
+
+
 results = mq.index("my-first-index").search(
-    q="Where to go today?"
+    q="Sam"
 )
 
 pprint.pprint(results)
